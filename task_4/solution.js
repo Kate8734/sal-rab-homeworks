86bd289e03e8028330d0d6a5d7c95291172e34d9
Ekaterina Beykina <kateb_90@mail.ru>
2022-03-20 18:49:38 +0300

    task 4 dane

--- task_4/solution.js
+++ task_4/solution.js
@@ -26,7 +26,7 @@ function renderProductsCards(json) {
     clearProducts();
     let products;
     products = parseProducts(json);
-    length.products.length
+    length.products.length;
 }
     for (let i = 0; i < length; i += 1) {
   addProduct (products[i]);
